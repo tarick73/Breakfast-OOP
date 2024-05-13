775e16afee81eb85cb26a7972c77f6daedf70528
Tarick <90559475+tarick73@users.noreply.github.com>
2024-05-13 21:05:04 +0200

    Wrote class Liquid

--- src/eatable/Liquid.java
+++ src/eatable/Liquid.java
@@ -1,5 +1,23 @@
 package eatable;
 
+import java.util.ArrayList;
+
 public class Liquid {
     protected String bevarage;
+    protected int kcal;
+    protected int mililiters;
+
+    public Liquid(String bevarage, int kcal, int mililiters) {
+        this.bevarage = bevarage;
+        this.kcal = kcal;
+        this.mililiters = mililiters;
+    }
+
+    public static int TotalKcal(Liquid beverage){
+        return beverage.kcal;
+    }
+
+    public static int TotalWeight(Liquid beverage){
+        return beverage.mililiters;
+    }
 }
