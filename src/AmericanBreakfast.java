f8756eb45ab6499b199e629f88ef8eca80c9c3a9
Tarick <90559475+tarick73@users.noreply.github.com>
2024-05-13 02:18:08 +0200

    End for today

--- src/AmericanBreakfast.java
+++ src/AmericanBreakfast.java
@@ -5,6 +5,14 @@ import java.util.ArrayList;
 public class AmericanBreakfast extends Breakfast {
     public AmericanBreakfast(){
         ArrayList<Food> MainDish=new ArrayList<>();
+        Food egg = new Food("Egg",185, 120);
+        MainDish.add(egg);
+
+        Food bacon = new Food("Bacon",172, 13.36);
+        MainDish.add(bacon);
+
+        Food pancakes = new Food("Pancakes",172, 13.36);
+        MainDish.add(pancakes);
     }
     @Override
     void serving() {
--- src/eatable/Food.java
+++ src/eatable/Food.java
@@ -3,15 +3,21 @@ import java.util.ArrayList;
 
 public class Food {
     protected String name;
-    protected int kkcal;
+    protected int kcal;
     protected double weight;
-    public Food(String name, int kkcal, double weight) {
+    public Food(String name, int kcal, double weight) {
         this.name = name;
-        this.kkcal = kkcal;
+        this.kcal = kcal;
         this.weight = weight;
     }
-
     protected ArrayList<Food> MainDish;
 
+    int countKcal(){
+
+        for(int i = 0; i<MainDish.size(); i++){
+
+        }
+        return kcal;
+    }
 
 }
